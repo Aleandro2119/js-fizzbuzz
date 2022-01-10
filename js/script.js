5a3bb7d57f5631e60efc9066fdd1f63ff5aecb99
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-10 13:10:46 +0100

    Add FizzBuzz

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
         console.log('Fizz')
-    }
-    else if (i % 5 === 0) {
+    } else if (i % 5 === 0) {
         console.log('Buzz')
+    } else {
+        console.log(i)
     }
-    console.log(i)
 }
